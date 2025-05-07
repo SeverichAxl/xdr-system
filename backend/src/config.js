61d13d373632efa7b0e24be4790bd5e33eb54596
JesusRodrigoV <jaicelvelasco105@gmail.com>
2025-05-07 18:13:37 -0400

    fix: Arreglo de variables de entorno, Chatbot funcionando

--- backend/src/config.js
+++ backend/src/config.js
@@ -1,7 +1,10 @@
+import dotenv from "dotenv";
+dotenv.config();
+
 export default {
   gemini: {
     API_KEY: process.env.GEMINI_API_KEY,
-    MODEL: process.env.GEMINI_MODEL,
+    MODEL: process.env.GEMINI_MODEL || "gemini-2.0-flash",
     apiVersion: "v1",
   },
 };
